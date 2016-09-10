3fa18f5cc02805cce67465b02ced3716c2de28aa
ompl <nlb3@rice.edu>
2016-09-09 21:23:50 -0500

    Finished circles

--- project2_template/CollisionChecking.cpp
+++ project2_template/CollisionChecking.cpp
@@ -19,9 +19,35 @@ bool isValidPoint(double x, double y, const std::vector<Rectangle>& obstacles)
 // lies outside of all obstacles, return true
 bool isValidCircle(double x, double y, double radius, const std::vector<Rectangle>& obstacles)
 {
-    // IMPLEMENT ME!
+    for (unsigned int i = 0; i < obstacles.size(); i++)
+    {
+    	double x_min = obstacles[i].x;
+    	double x_max = obstacles[i].x + obstacles[i].width;
+    	double y_min = obstacles[i].y;
+    	double y_max = obstacles[i].y + obstacles[i].height;
+    	double vertex_distances[4];
+    	vertex_distances[0] = sqrt(pow(x-x_min,2) + pow(y-y_min,2));
+    	vertex_distances[1] = sqrt(pow(x-x_min,2) + pow(y-y_max,2));
+    	vertex_distances[2] = sqrt(pow(x-x_max,2) + pow(y-y_min,2));
+    	vertex_distances[3] = sqrt(pow(x-x_max,2) + pow(y-y_max,2));
+    	if ((x_min - radius <= x) && (x <= x_max + radius) && (y_min <= y) && (y <= y_max))
+    	{
+    		return false;
+    	}
+    	if ((x_min <= x) && (x <= x_max) && (y_min - radius <= y) && (y <= y_max + radius))
+    	{
+    		return false;
+    	}
+    	for (int i = 0; i < 4; i++)
+    	{
+    		if (vertex_distances[i] <= radius)
+    		{
+    			return false;
+    		}
+    	}
+    }
 
-    return false;
+    return true;
 }
 
 // Intersect a square with center at (x,y), orientation theta, and the given side length with the set of rectangles.
